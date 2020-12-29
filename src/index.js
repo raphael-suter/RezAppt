5df6a33e7630a5f95765b7d00cf7956c73e92a70
Raphael Suter <raphael.werner.suter@gmail.com>
2020-12-29 17:14:08 +0100

    Set background image

--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./themes/GlobalStyle";
 import LightTheme from "./themes/LightTheme";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <ThemeProvider theme={LightTheme}>
-      <GlobalStyle />
-      <h1>Hello World</h1>
-    </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+const App = () => (
+  <ThemeProvider theme={LightTheme}>
+    <GlobalStyle />
+    <h1>RezAppt</h1>
+  </ThemeProvider>
 );
+
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/themes/GlobalStyle.js
+++ src/themes/GlobalStyle.js
@@ -5,4 +5,10 @@ export default createGlobalStyle`
     margin: 0;
     padding: 0;
   }
+
+  body {
+    height: 100vh;
+    background: no-repeat center center / cover;
+    background-image: url(${process.env.PUBLIC_URL}/background.jpg);
+  }
 `;
