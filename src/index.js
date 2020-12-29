5828d0f46c2b070732180bc333baff407e00a4f1
Raphael Suter <raphael.werner.suter@gmail.com>
2020-12-29 16:49:21 +0100

    Add theme and global style

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./themes/GlobalStyle";
+import LightTheme from "./themes/LightTheme";
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Hello World</h1>
+    <ThemeProvider theme={LightTheme}>
+      <GlobalStyle />
+      <h1>Hello World</h1>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ src/themes/LightTheme.js
@@ -0,0 +1,9 @@
+const LightTheme = {
+  mainColor: "#FFFFFF",
+  lightColor: "#67D07E",
+  mediumColor: "#FACD66",
+  darkColor: "#C4C4C4",
+  shadowColor: "rgba(0, 0, 0, 0.4)",
+};
+
+export default LightTheme;
